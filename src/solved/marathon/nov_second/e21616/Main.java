2a6d2a0f31034f4a4ec75b1ccd06b642889f017b
lhs <fantasytotal@naver.com>
2024-11-16 20:54:39 +0900

    24.11.16

---
+++ src/solved/marathon/nov_second/e21616/Main.java
@@ -0,0 +1,34 @@
+package solved.marathon.nov_second.e21616;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int k = Integer.parseInt(bufferedReader.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < k; i++) {
+            String command = bufferedReader.readLine();
+            if (set.contains(command)) {
+                set.remove(command);
+            } else {
+                set.add(command);
+            }
+        }
+        int r = 0, c = 0;
+        for (String s : set) {
+            if (s.startsWith("R"))
+                r++;
+            else
+                c++;
+        }
+        System.out.println(r * n + c * m - r * c * 2);
+        bufferedReader.close();
+    }
+}
